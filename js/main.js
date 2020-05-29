f45689f87de074e54e3512033ff5949e786b557e
n2 <7worki.nprogress@gmail.com>
2020-05-29 21:00:32 +0900

    #08 activeクラスを付け替えよう

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@
 {
     const menuItems = document.querySelectorAll('.menu li a');
 
-    menuItems.forEach(item => {
-        item.addEventListener('click', e => {
+    menuItems.forEach(click => {
+        click.addEventListener('click', e => {
             e.preventDefault(); //a要素のもともと持っているページ遷移の動作をなくす
 
-            item.classList.add('active');
+            menuItems.forEach(item =>{
+                item.classList.remove('active');
+            });
+
+            click.classList.add('active');
         })
     });
 }
